6097b9e69d2cc560a0adafab9e8dd69adb277387
RyosukeDTomita <d310151c2@gmail.com>
2024-12-09 02:32:30 +0900

    feat: basic_auth

--- reverse_proxy/src/main.lua
+++ reverse_proxy/src/main.lua
@@ -1,15 +1,11 @@
---local basic_auth = require "basic_auth"
+local basic_auth = require "basic_auth"
 
--- nginx.confのupstreamブロックを指定してload balance
-local transfer_ip = "proxied_server"
--- proxy_passにupstreamを指定する場合は，Host headerの上書きが必要
-ngx.var.host_header = "abehiroshi.la.coocan.jp"
--- local transfer_ip = "abehiroshi.la.coocan.jp"
--- local transfer_ip = "example.com"
+-- proxy_passを動的に設定する
+local transfer_host = "https://example.com"
+-- local transfer_host = "http://abehiroshi.la.coocan.jp"
 local transfer_path = "/"
+ngx.var.pass = transfer_host .. transfer_path
 
-ngx.var.pass = "http://" .. transfer_ip .. transfer_path
+basic_auth.auth()
 
--- basic_auth.basic_auth()
-
-ngx.log(ngx.INFO, "request_url: ", ngx.var.pass);
+-- ngx.log(ngx.INFO, "request_url: ", ngx.var.pass);
