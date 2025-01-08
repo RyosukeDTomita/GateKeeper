430d9a0bccf8b32de25f33de8c437474b61ab18f
RyosukeDTomita <d310151c2@gmail.com>
2025-01-09 00:56:52 +0900

    refactor: auth module

--- reverse_proxy/src/auth/basic_auth.lua
+++ reverse_proxy/src/auth/basic_auth.lua
@@ -3,13 +3,11 @@ local resty_redis = require "resty.redis"
 local redis = resty_redis:new()
 
 
--- Authorizationヘッダがないならログインのポップアップを出す
-local function is_authorization_header()
-    if not ngx.var.http_Authorization then
-        -- Basic認証のポップアップを出す。
-        ngx.header["WWW-Authenticate"] = 'Basic realm="' .. ngx.var.host .. '/basic Restricted"'
-        ngx.exit(ngx.HTTP_UNAUTHORIZED)
-    end
+-- WWW-Authenticateヘッダを返すhelper関数
+local function return_authorization_header()
+    -- Basic認証のポップアップを出す。
+    ngx.header["WWW-Authenticate"] = 'Basic realm="' .. ngx.var.host .. '/basic Restricted"'
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
 
 
@@ -51,7 +49,9 @@ end
 
 
 function _M.auth()
-    is_authorization_header()
+    if not ngx.var.http_Authorization then
+        return_authorization_header()
+    end
 
     local user_id, password = decode_userid_and_password()
     ngx.log(ngx.INFO, "TRYING TO LOGIN: ", user_id)
@@ -61,10 +61,8 @@ function _M.auth()
         ngx.log(ngx.INFO, "LOGIN SUCCESS: ", user_id)
         return --NOTE: ngx.exit(ngx.HTTP_OK)を返すと，後続のコンテンツが表示されない
     else
-        --認証失敗時には再度Basic認証のポップアップを出す
+        return_authorization_header()
         ngx.log(ngx.INFO, "LOGIN FAILED: ", user_id)
-        ngx.header["WWW-Authenticate"] = 'Basic realm="' .. ngx.var.host .. '/basic Restricted"'
-        ngx.exit(ngx.HTTP_UNAUTHORIZED)
     end
 end
 return _M
