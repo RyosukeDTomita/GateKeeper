8a87441114f4816667111f4b5128a3206e5de817
RyosukeDTomita <d310151c2@gmail.com>
2025-01-22 15:56:02 +0900

    feat: form authentication

--- redis/initial_data_redis.sh
+++ redis/initial_data_redis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# user data
 redis-cli -h redis_app -p 6379 SET "USER|user" "password"
+
+# ACL
 redis-cli -h redis_app -p 6379 HSET "ACL|basic" "proxy_pass" "https://example.com" "authentication_type" "basic"
 redis-cli -h redis_app -p 6379 HSET "ACL|digest" "proxy_pass" "https://example.com" "authentication_type" "digest"
 redis-cli -h redis_app -p 6379 HSET "ACL|form" "proxy_pass" "https://example.com" "authentication_type" "form"
